05f64cc2ba1c7fdd9aa5998ac02248c1cf60de6e
WilliamMcRoberts <wamcroberts2@gmail.com>
2024-08-25 13:29:06 -0400

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,94 @@
 use std::{
     alloc::{alloc, dealloc, handle_alloc_error, realloc, Layout},
-    mem::{self, ManuallyDrop},
+    mem::{self},
     ops::{Deref, DerefMut},
     ptr::{copy, read, write, NonNull},
 };
 
 #[allow(dead_code)]
 struct Vector<T> {
-    ptr: NonNull<T>,
+    buf: RawVec<T>,
     len: usize,
-    capacity: usize,
 }
 
 #[allow(dead_code)]
 impl<T> Vector<T> {
+    pub fn push(&mut self, elem: T) {
+        if self.len == self.capacity() {
+            self.buf.grow();
+        }
+
+        unsafe {
+            write(self.ptr().add(self.len), elem);
+        }
+
+        // Can't fail, we'll OOM first.
+        self.len += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.len == 0 {
+            None
+        } else {
+            self.len -= 1;
+            unsafe { Some(read(self.ptr().add(self.len))) }
+        }
+    }
+
+    pub fn insert(&mut self, index: usize, elem: T) {
+        // Note: `<=` because it's valid to insert after everything
+        // which would be equivalent to push.
+        assert!(index <= self.len, "index out of bounds");
+        if self.len == self.capacity() {
+            self.buf.grow();
+        }
+
+        unsafe {
+            // ptr::copy(src, dest, len): "copy from src to dest len elems"
+            copy(
+                self.ptr().add(index),
+                self.ptr().add(index + 1),
+                self.len - index,
+            );
+            write(self.ptr().add(index), elem);
+        }
+
+        self.len += 1;
+    }
+
+    pub fn remove(&mut self, index: usize) -> T {
+        // Note: `<` because it's *not* valid to remove after everything
+        assert!(index < self.len, "index out of bounds");
+        unsafe {
+            self.len -= 1;
+            let result = read(self.ptr().add(index));
+            copy(
+                self.ptr().add(index + 1),
+                self.ptr().add(index),
+                self.len - index,
+            );
+            result
+        }
+    }
+
+    fn ptr(&self) -> *mut T {
+        self.buf.ptr.as_ptr()
+    }
+
+    fn capacity(&self) -> usize {
+        self.buf.capacity
+    }
+
+    pub fn new() -> Self {
+        Vector {
+            buf: RawVec::new(),
+            len: 0,
+        }
+    }
+}
+
+#[allow(dead_code)]
+impl<T> RawVec<T> {
     pub fn new() -> Self {
         assert!(
             mem::size_of::<T>() != 0,
@@ -21,7 +96,6 @@ impl<T> Vector<T> {
         );
         Self {
             ptr: NonNull::dangling(),
-            len: 0usize,
             capacity: 0usize,
         }
     }
@@ -61,78 +135,11 @@ impl<T> Vector<T> {
         };
         self.capacity = new_cap;
     }
-
-    pub fn push(&mut self, elem: T) {
-        if self.len == self.capacity {
-            self.grow();
-        }
-
-        unsafe {
-            write(self.ptr.as_ptr().add(self.len), elem);
-        }
-
-        // Can't fail, we'll OOM first.
-        self.len += 1;
-    }
-
-    pub fn pop(&mut self) -> Option<T> {
-        if self.len == 0 {
-            None
-        } else {
-            self.len -= 1;
-            unsafe { Some(read(self.ptr.as_ptr().add(self.len))) }
-        }
-    }
-
-    pub fn insert(&mut self, index: usize, elem: T) {
-        // Note: `<=` because it's valid to insert after everything
-        // which would be equivalent to push.
-        assert!(index <= self.len, "index out of bounds");
-        if self.len == self.capacity {
-            self.grow();
-        }
-
-        unsafe {
-            // ptr::copy(src, dest, len): "copy from src to dest len elems"
-            copy(
-                self.ptr.as_ptr().add(index),
-                self.ptr.as_ptr().add(index + 1),
-                self.len - index,
-            );
-            write(self.ptr.as_ptr().add(index), elem);
-        }
-
-        self.len += 1;
-    }
-
-    pub fn remove(&mut self, index: usize) -> T {
-        // Note: `<` because it's *not* valid to remove after everything
-        assert!(index < self.len, "index out of bounds");
-        unsafe {
-            self.len -= 1;
-            let result = read(self.ptr.as_ptr().add(index));
-            copy(
-                self.ptr.as_ptr().add(index + 1),
-                self.ptr.as_ptr().add(index),
-                self.len - index,
-            );
-            result
-        }
-    }
-
-    pub fn capacity(&self) -> usize {
-        self.capacity
-    }
-
-    pub fn len(&self) -> usize {
-        self.len
-    }
 }
 
-impl<T> Drop for Vector<T> {
+impl<T> Drop for RawVec<T> {
     fn drop(&mut self) {
         if self.capacity != 0 {
-            while let Some(_) = self.pop() {}
             let layout = Layout::array::<T>(self.capacity).unwrap();
             unsafe {
                 dealloc(self.ptr.as_ptr() as *mut u8, layout);
@@ -144,20 +151,24 @@ impl<T> Drop for Vector<T> {
 impl<T> Deref for Vector<T> {
     type Target = [T];
     fn deref(&self) -> &[T] {
-        unsafe { std::slice::from_raw_parts(self.ptr.as_ptr(), self.len) }
+        unsafe { std::slice::from_raw_parts(self.ptr(), self.len) }
     }
 }
 
 impl<T> DerefMut for Vector<T> {
     fn deref_mut(&mut self) -> &mut [T] {
-        unsafe { std::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.len) }
+        unsafe { std::slice::from_raw_parts_mut(self.ptr(), self.len) }
     }
+}
+
+pub struct RawVec<T> {
+    ptr: NonNull<T>,
+    capacity: usize,
 }
 
 #[allow(dead_code)]
 pub struct IntoIter<T> {
-    buf: NonNull<T>,
-    cap: usize,
+    _buf: RawVec<T>, // we don't actually care about this. Just need it to live.
     start: *const T,
     end: *const T,
 }
@@ -166,24 +177,21 @@ impl<T> IntoIterator for Vector<T> {
     type Item = T;
     type IntoIter = IntoIter<T>;
     fn into_iter(self) -> IntoIter<T> {
-        // Make sure not to drop Vec since that would free the buffer
-        let vec = ManuallyDrop::new(self);
-
-        // Can't destructure Vec since it's Drop
-        let ptr = vec.ptr;
-        let cap = vec.capacity;
-        let len = vec.len;
+        // need to use ptr::read to unsafely move the buf out since it's
+        // not Copy, and Vec implements Drop (so we can't destructure it).
+        let buf = unsafe { read(&self.buf) };
+        let len = self.len;
+        mem::forget(self);
 
         IntoIter {
-            buf: ptr,
-            cap,
-            start: ptr.as_ptr(),
-            end: if cap == 0 {
-                // can't offset off this pointer, it's not allocated!
-                ptr.as_ptr()
+            start: buf.ptr.as_ptr(),
+            end: if buf.capacity == 0 {
+                // can't offset off of a pointer unless it's part of an allocation
+                buf.ptr.as_ptr()
             } else {
-                unsafe { ptr.as_ptr().add(len) }
+                unsafe { buf.ptr.as_ptr().add(len) }
             },
+            _buf: buf,
         }
     }
 }
@@ -223,19 +231,14 @@ impl<T> DoubleEndedIterator for IntoIter<T> {
 
 impl<T> Drop for IntoIter<T> {
     fn drop(&mut self) {
-        if self.cap != 0 {
-            // drop any remaining elements
-            for _ in &mut *self {}
-            let layout = Layout::array::<T>(self.cap).unwrap();
-            unsafe {
-                dealloc(self.buf.as_ptr() as *mut u8, layout);
-            }
-        }
+        // only need to ensure all our elements are read;
+        // buffer will clean itself up afterwards.
+        for _ in &mut *self {}
     }
 }
 
-unsafe impl<T: Send> Send for Vector<T> {}
-unsafe impl<T: Sync> Sync for Vector<T> {}
+unsafe impl<T: Send> Send for RawVec<T> {}
+unsafe impl<T: Sync> Sync for RawVec<T> {}
 
 #[cfg(test)]
 mod tests {
@@ -246,9 +249,13 @@ mod tests {
         let mut v: Vector<usize> = Vector::new();
         v.push(16 as usize);
         assert_eq!(v.len(), 1);
-        assert_eq!(v.capacity(), 4);
+        assert_eq!(v.capacity(), 1);
         v.push(20 as usize);
+        assert_eq!(v.len(), 2);
+        assert_eq!(v.capacity(), 2);
         v.push(21 as usize);
+        assert_eq!(v.len(), 3);
+        assert_eq!(v.capacity(), 4);
         v.push(23 as usize);
         assert_eq!(v.len(), 4);
         assert_eq!(v.capacity(), 4);
